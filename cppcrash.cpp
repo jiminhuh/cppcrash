356cbcdd44e80ed885407dbca7a3267b9c4b37e7
Jimin Huh <jiminhuh95@gmail.com>
2020-10-30 23:23:35 -0500

    enum practice

---
+++ cppcrash.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+// enum EyeColor {Brown, Blue, Green, Grey, Other};
+
+int main() {
+
+    // EyeColor eyeColor = Brown; 
+
+    // switch (99) {
+    //     case Brown: cout << "Yay me too";break;
+    //     case Blue: cout << "My favorite Color";break;
+    //     case Green: cout << "Nice";break;
+    //     case Grey: cout << "Weird";break;
+    //     case Other: cout << "Interesting."; break;
+    //     default: cout << "No Way"; break;
+    // }
+
+}
